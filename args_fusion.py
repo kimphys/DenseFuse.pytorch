7fc2bc485d815e83aa6aa08c9d3bbd426c280506
kimphys <kim.sehoon1125@gmail.com>
2020-09-27 23:34:49 +0900

    Support the latest version of PyTorch

--- args_fusion.py
+++ args_fusion.py
@@ -4,11 +4,11 @@ class args():
 	epochs = 4 #"number of training epochs, default is 2"
 	save_per_epoch = 1
 	batch_size = 4 #"batch size for training, default is 4"
-	dataset = "/home/kim/ai/dataset/train.txt"
+	dataset = "./train.txt"
 	HEIGHT = 256
 	WIDTH = 256
 
-	save_model_dir = "models/" #"path to folder where trained model with checkpoints will be saved."
+	save_model_dir = "./models/" #"path to folder where trained model with checkpoints will be saved."
 
 	image_size = 256 #"size of training images, default is 256 X 256"
 	ssim_weight = [1,10,100,1000,10000]
@@ -16,7 +16,7 @@ class args():
 	lr = 1e-4 #"learning rate, default is 0.001"	
 	resume = None # if you have, please put the path of the model like "./models/densefuse_gray.model"
 
-	args.strategy_type = 'addition'
+	strategy_type = 'addition'
 	test_save_dir = "results/"
-	test_img = "/home/kim/ai/dataset/img.txt"
-	test_ir = "/home/kim/ai/dataset/ir.txt"
+	test_img = "./img.txt"
+	test_ir = "./ir.txt"
